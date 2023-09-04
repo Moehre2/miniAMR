3f9c10a50c46bf811b37ade0faf7c653c9d27c66
Moritz Timmer <25349452+Moehre2@users.noreply.github.com>
2023-09-04 16:30:57 +0200

    Benchmark test worked, now real benchmark

--- benchmark_openmp.sh
+++ benchmark_openmp.sh
@@ -4,12 +4,12 @@
 #SBATCH --job-name=miniAMR_benchmark_openmp
 #SBATCH --account=thes1472
 #SBATCH --output=output_%j.txt
-#SBATCH --time=01:00:00
-#SBATCH --ntasks=2
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=16
 #SBATCH --cpus-per-task=48
 #SBATCH --exclusive
 
-repetitions=2
+repetitions=6
 
 source setupenv.sh
 
@@ -20,5 +20,6 @@ cd openmp
 make clean
 make
 for i in $(seq $repetitions); do
-    OMP_NUM_THREADS=48 mpirun -np 2 ./miniAMR.x --num_refine 4 --max_blocks 4000 --init_x 1 --init_y 1 --init_z 1 --npx 2 --npy 1 --npz 1 --nx 8 --ny 8 --nz 8 --num_objects 2 --object 2 0 -1.10 -1.10 -1.10 0.030 0.030 0.030 1.5 1.5 1.5 0.0 0.0 0.0 --object 2 0 0.5 0.5 1.76 0.0 0.0 -0.025 0.75 0.75 0.75 0.0 0.0 0.0 --num_tsteps 20 --checksum_freq 4 --stages_per_ts 8 > ../benchmarks/openmp/${i}.txt
+    echo "Benchmark $i of $repetitions"
+    OMP_NUM_THREADS=48 mpirun -np 16 ./miniAMR.x --num_refine 4 --max_blocks 4000 --init_x 1 --init_y 1 --init_z 1 --npx 4 --npy 2 --npz 2 --nx 8 --ny 8 --nz 8 --num_objects 2 --object 2 0 -1.10 -1.10 -1.10 0.030 0.030 0.030 1.5 1.5 1.5 0.0 0.0 0.0 --object 2 0 0.5 0.5 1.76 0.0 0.0 -0.025 0.75 0.75 0.75 0.0 0.0 0.0 --num_tsteps 100 --checksum_freq 4 --stages_per_ts 16 > ../benchmarks/openmp/${i}.txt
 done
